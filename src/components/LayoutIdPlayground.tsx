e445192a53fe24ff6d14fb6411b5c803f477d32c
Tikhon Belousko <tikhon.belousko@gmail.com>
2025-10-05 12:59:40 +0100

    layout id animation

--- src/components/LayoutIdPlayground.tsx
+++ src/components/LayoutIdPlayground.tsx
@@ -1,9 +1,50 @@
-import { motion } from "motion/react";
+import { motion, AnimatePresence } from "motion/react";
+import { useState } from "react";
 
 export function LayoutIdPlayground() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div>
-      <motion.div layoutId="box" className="size-10 bg-red-500" />
+    <div className="flex items-center justify-center h-screen w-screen bg-gray-2">
+      <AnimatePresence mode="popLayout">
+        {!isOpen && (
+          <motion.div
+            layoutId="box"
+            className="relative w-[200px] h-[48px] bg-white border-[0.5px] border-black/20 overflow-hidden"
+            transition={{
+              duration: 1,
+            }}
+            onClick={() => {
+              setIsOpen(true);
+            }}
+            style={{
+              borderWidth: "0.5px",
+              borderRadius: "16px",
+              boxShadow: "0 0 10px 0 rgba(0, 0, 0, 0.2)",
+            }}
+          ></motion.div>
+        )}
+      </AnimatePresence>
+
+      <AnimatePresence mode="popLayout">
+        {isOpen && (
+          <motion.div
+            layoutId="box"
+            className="w-[300px] h-[600px] bg-white border-[0.5px] border-black/20"
+            transition={{
+              duration: 1,
+            }}
+            onClick={() => {
+              setIsOpen(false);
+            }}
+            style={{
+              borderWidth: "0.5px",
+              borderRadius: "16px",
+              boxShadow: "0 0 10px 0 rgba(0, 0, 0, 0.2)",
+            }}
+          />
+        )}
+      </AnimatePresence>
     </div>
   );
 }
